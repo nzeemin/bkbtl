1bad09165e5ef32ed9c5c62e54c22f6ea2fd8428
nzeemin <nzeemin@0dc07406-d219-11de-a205-5990a31c5447>
2009-12-05 06:25:42 +0000

    [*] Translate_BK_Unicode: conversion codes FIXed.

--- trunk/src/BKBTL/Common.h
+++ trunk/src/BKBTL/Common.h
@@ -59,7 +59,7 @@ BOOL ParseOctalValue(LPCTSTR text, WORD* pValue);
 void DrawOctalValue(HDC hdc, int x, int y, WORD value);
 void DrawBinaryValue(HDC hdc, int x, int y, WORD value);
 BOOL ParseOctalValue(LPCTSTR text, WORD* pValue);
-TCHAR Translate_KOI8R_Unicode(BYTE ch);
+TCHAR Translate_BK_Unicode(BYTE ch);
 void DrawCharKOI8R(HDC hdc, int x, int y, BYTE ch);
 
 LPCTSTR GetFileNameFromFilePath(LPCTSTR lpfilepath);
